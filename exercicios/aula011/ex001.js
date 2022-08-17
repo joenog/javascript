253e7de78cd0403b6443d9960daecf35f0d4da57
Joe Nogueira <joabnogueira@outlook.com>
2022-08-17 08:40:47 -0300

    Feito ex003 condicionais

---
+++ exercicios/aula011/ex001.js
@@ -0,0 +1,6 @@
+var vel = 500
+console.log(`A valocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {  // condicao simples
+    console.log(`Você utrapassou a velocidade permitida, MUTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicios/aula011/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'    // string sempre entre aspas ''
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') {
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
