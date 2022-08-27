6746f21899669b973e62e6ca0cd802ee327caba7
Joe Nogueira <joabnogueira@outlook.com>
2022-08-27 17:02:46 -0300

    Adicionei fav

--- exercicios/aula012ex/script.js
+++ exercicios/aula012ex/script.js
@@ -1,20 +1,22 @@
-var msg = document.querySelector('#msg')
-var img = document.querySelector('#img')
-var data = new Date()
-var hora = data.getHours()
-var min = data.getMinutes()
+function vai() {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#img')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    
+    msg.innerHTML = `${hora} : ${min}`
 
-msg.innerHTML = `${hora} : ${min}`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'images/manha.png'
+        document.body.style.background = '#62b4f4'
 
-if (hora >= 0 && hora < 12) {
-    img.src = 'images/manha.png'
-    document.body.style.background = '#62b4f4'
+    } else if (hora >= 12  && hora <= 18) {
+        img.src = 'images/tarde.png'
+        document.body.style.background = '#f9b682'
 
-} else if (hora >= 12  && hora <= 18) {
-    img.src = 'images/tarde.png'
-    document.body.style.background = '#f9b682'
-
-} else {
-    img.src = 'images/noite.png'
-    document.body.style.background = '#1e72b0'
+    } else {
+        img.src = 'images/noite.png'
+        document.body.style.background = '#1e72b0'
+    }
 }
