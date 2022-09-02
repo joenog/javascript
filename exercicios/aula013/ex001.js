c9718f0913169aa1431c31c9891b3e47f9bcfb08
Joe Nogueira <joabnogueira@outlook.com>
2022-09-02 09:00:35 -0300

    Criado ex013

---
+++ exercicios/aula013/ex001.js
@@ -0,0 +1,21 @@
+var c = 1 
+while (c <= 3) {
+    console.log(`Passos ${c}`)
+    c++
+}
+
+var d = 1 
+do {
+    console.log(`DO-WHILE ${d}`)
+    d++
+} while (d <=3)
+
+/* (haverá casos e casos e para isso temos as duas opções)
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
